16475dd78440444ed7e1fe4e24cb4b34bf6b2a69
Peter Ren <lcdcdr2004@gmail.com>
2018-04-27 18:27:58 +0800

    Added mnesia support for distillery

--- server/dumb_note/mix.exs
+++ server/dumb_note/mix.exs
@@ -14,7 +14,7 @@ defmodule DumbNote.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :mnesia],
       applications: [:wobserver],
       mod: {DumbNote, []},
     ]
